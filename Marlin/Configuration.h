99522cc2b698aadc81f44c4d86665870d2ca6361
kcheeeung <k.cheung@berkeley.edu>
2017-02-15 18:29:37 -0800

    We upgraded the motors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -486,7 +486,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
