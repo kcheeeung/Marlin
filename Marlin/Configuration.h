7f579a276f0839a4b74635fda1bb84666d2f910b
kcheeeung <k.cheung@berkeley.edu>
2017-04-05 21:02:24 -0700

    slow e step rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,7 +493,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3200, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3200, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
